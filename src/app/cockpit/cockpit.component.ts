5a5ec414a0a374b5e143970737864a0d048829a3
asubedi <asubedi@go.olemiss.edu>
2023-06-20 19:23:52 -0500

    lifecycle hooks

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,12 +8,14 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   //serverElements = [];
   // newServerName = '';
-  newServerContent = '';
+  //newServerContent = '';
 
   // @Output decorator indicates that these properties are output properties (emit events to the parent component)
   // serverCreated and bluePrintCreated event will emit an object with serverName and serverContent properties
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output() bluePrintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+
+  @ViewChild('serverContentInput', { static: true }) serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -22,12 +24,15 @@ export class CockpitComponent implements OnInit {
 
   // Emit the serverCreated event with the new server name and content
   onAddServer(nameInput: HTMLInputElement) {
-    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.newServerContent });
+    //console.log(this.serverContentInput);
+    this.serverCreated.emit({ serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value });
+
   }
 
   // Emit the bluePrintCreated event with the new server name and content
   onAddBlueprint(contentInput: HTMLInputElement) {
-    this.bluePrintCreated.emit({ serverName: contentInput.value, serverContent: this.newServerContent })
+    //console.log(this.serverContentInput);
+    this.bluePrintCreated.emit({ serverName: contentInput.value, serverContent: this.serverContentInput.nativeElement.value })
   }
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,22 +1,93 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  //encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
+
 // binding through an alias ('serverElement)
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+
   @Input('serverElement') element: {
     type: string,
     name: string,
     content: string
   }
 
-  constructor() { }
+  @ViewChild('heading', { static: true }) header: ElementRef;
 
+
+  constructor() {
+    console.log("constructor called")
+  }
+
+  // called after a bound input property ngOnChanges
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ng On changes called");
+    console.log(changes);
+  }
+
+  // called once the component is initialized
   ngOnInit() {
+    console.log("ngOnInit called");
+
+    // expecting empty -  can't check value of element because it's not rendered yet
+    console.log("Text content: " + this.header.nativeElement.textContent);
+  }
+
+  // called during every detection run
+  ngDoCheck() {
+    console.log("ngDoCheckCalled");
+  }
+
+  // called after content (ng-content) has been projected into view
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+  }
+
+  // called everytime the projected content has been ngAfterContentChecked
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called");
+    console.log("Text content: " + this.header.nativeElement.textContent); // expecting test1
+  }
+
+  // alled everytime the view (and child views) has been ngAfterViewChecked
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called");
+  }
+
+  // called once the component is about to be destroyed
+  ngOnDestroy() {
+    console.log("ngOnDestroy callled");
   }
 
 }
